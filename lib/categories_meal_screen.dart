51593f8a9a451c32d6218d470160447a0742cc29
William Maphanga <williesdk@gmail.com>
2019-08-31 05:48:03 +0200

    added meals to categories

--- lib/categories_meal_screen.dart
+++ lib/categories_meal_screen.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
+import './dummy-data.dart';
 
 class CategoriesMealScreen extends StatelessWidget {
+  static const routeName = '/categories-meals';
+
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+
+    final String categoryTitle = routeArgs['title'];
+    final String categoryId = routeArgs['id'];
+    final categoriesMeal = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
-        child: Text('The recipe category'),
+        child: ListView.builder(
+          itemBuilder: (ctx, index) {
+            return Text(categoriesMeal[index].title);
+          },
+          itemCount: categoriesMeal.length,
+        ),
       ),
     );
   }
