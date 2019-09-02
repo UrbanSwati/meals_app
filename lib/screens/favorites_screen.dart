e8f6ebf58b8a168be649741d64f26c2d21f8d7f6
William Maphanga <williesdk@gmail.com>
2019-09-02 21:13:08 +0200

    added constructor in fav screen
    initials list of fav meals

--- lib/screens/favorites_screen.dart
+++ lib/screens/favorites_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
 
 class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
 
+  FavoritesScreen(this.favoriteMeals);
+  
   @override
   Widget build(BuildContext context) {
     return Center(child: Text('Favorites Screen'));
